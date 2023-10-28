286cfc6752ee56a67869a0e5f7d8a388d5d279aa
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2023-10-28 21:21:26 +0600

    company details UI

--- src/screens/CompanyDetails/index.tsx
+++ src/screens/CompanyDetails/index.tsx
@@ -1,32 +1,30 @@
 /* eslint-disable quotes */
-import React, {useEffect, useState} from 'react';
-import {View, Text, Image} from 'react-native';
-import {styles} from './styles';
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image } from 'react-native';
+import { styles } from './styles';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import request from 'graphql-request';
-import {getCompanyDocument, graphqlEndpoint} from '../../../config';
+import { getCompanyDocument, graphqlEndpoint } from '../../../config';
+import { useSelector } from 'react-redux';
+import useCompanyViewModel from '../../view-models/useCompanyViewModel';
 
-export const CompanyDetailsScreen = ({
-  navigation,
-  route: {params},
-}: NativeStackScreenProps<any>) => {
+export const CompanyDetailsScreen = () => {
+  const { currentCompany } = useCompanyViewModel()
   return (
     <View style={styles.container}>
-      <Text>{`To get the company details, you need to do a graphQL request to the endpoint ${graphqlEndpoint}, with the header:`}</Text>
-      <Text
-        style={
-          styles.boldText
-        }>{`'x-api-key': 'the-api-token-you-got-earlier'`}</Text>
-      <Text>and the document:</Text>
-      <Text>{getCompanyDocument}</Text>
-      <Text>
-        Along with the company's ID you pressed from the previous screen passed
-        as a variable
-      </Text>
-      <Text>
-        You can use the package graphql-request for this, or any other package
-        you choose.
-      </Text>
+      <Image
+        source={{ uri: currentCompany.coverImageUrl }}
+        style={styles.coverImage}
+        resizeMode='contain'
+      />
+      <Image
+        source={{ uri: currentCompany.logoUrl }}
+        style={styles.profileImage}
+        resizeMode='contain'
+      />
+      <Text style={styles.boldText}>{currentCompany.name}</Text>
+      <Text style={styles.desc}>{`${currentCompany.city},${currentCompany.country}`}</Text>
+      <Text style={styles.desc} >{currentCompany.description}</Text>
     </View>
   );
 };
--- src/screens/CompanyDetails/styles.ts
+++ src/screens/CompanyDetails/styles.ts
@@ -1,15 +1,34 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
+import { colors, dimensions, fontSizes, fontWeights, styleGuide } from '../../styles/globalStyles';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 12,
+    padding: styleGuide.padding,
+    paddingTop: 0
   },
   text: {
-    margin: 10,
+    margin: styleGuide.padding,
   },
+  desc: { textAlign: 'center' },
   boldText: {
-    margin: 10,
+    margin: styleGuide.padding,
     fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 22
   },
+  coverImage: {
+    width: dimensions.fullWidth - styleGuide.padding * 2,
+    height: 200
+  },
+  profileImage: {
+    width: dimensions.fullWidth / 4,
+    height: dimensions.fullWidth / 4,
+    borderRadius: dimensions.fullWidth / 8,
+    marginTop: - dimensions.fullWidth / 8,
+    alignSelf: 'center',
+    borderWidth: 0.5,
+    borderColor: colors.primary
+
+  }
 });
