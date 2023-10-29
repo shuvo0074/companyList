18b58aa5d8d75720b12fdfd58a867a0a295eb032
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2023-10-29 23:46:29 +0600

    write test script for login form

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,17 +1,49 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
-import {App} from '../src/App';
+import {
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+} from '@testing-library/react-native';
+import LoginFormWithMemo from '../src/components/LoginForm';
+import { ReduxProvider } from '../src/App';
 
-// Note: import explicitly to use the types shiped with jest.
-import {it} from '@jest/globals';
+afterEach(cleanup);
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+jest.mock('@react-native-community/async-storage', () => ({
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+}));
+
+jest.mock('@react-navigation/native-stack', () => ({
+  createNativeStackNavigator: jest.fn(),
+}));
+
+it('fills in the form and handleSubmit is called', async () => {
+  const username = 'hi';
+  const password = 'qwerty1234';
+  // Create a mock function to pass as onSubmit prop
+  const handleSubmit = jest.fn();
+  // Render the component
+  render(
+    <ReduxProvider>
+      <LoginFormWithMemo onSubmit={handleSubmit} />
+    </ReduxProvider>
+  );
+
+  // Fill in the form and submit it
+  await fireEvent.changeText(
+    screen.getByPlaceholderText(/email/i),
+    username,
+  );
+  await fireEvent.changeText(
+    screen.getByPlaceholderText(/password/i),
+    password,
+  );
+  fireEvent.press(screen.getByText(/Sign In/i));
+
+  // Verify that handleSubmit was called with the correct arguments and only once
+  expect(handleSubmit).toHaveBeenCalledWith();
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
 });
